3aeabbc366636044b0117bb09413286927679c05
Joshua <jovanitz@gmail.com>
2017-07-12 16:58:29 -0500

    new changes

--- main.js
+++ main.js
@@ -3,11 +3,35 @@ import "babel-polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
-
+import createSagaMiddleware from 'redux-saga'
 import Counter from './Counter'
 import reducer from './reducers'
+import { delay } from 'redux-saga'
+import { put, takeEvery } from 'redux-saga/effects'
 
-const store = createStore(reducer)
+// Our worker Saga: will perform the async increment task
+function* incrementAsync() {
+  yield delay(1000)
+  yield put({ type: 'INCREMENT' })
+}
+
+// Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
+function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+);
+
+function* helloSaga() {
+  console.log('Hello Sagas!')
+}
+
+sagaMiddleware.run(watchIncrementAsync);
 
 const action = type => store.dispatch({type})
 
@@ -16,7 +40,8 @@ function render() {
     <Counter
       value={store.getState()}
       onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
+      onDecrement={() => action('DECREMENT')}
+      onIncrementAsync={() => action('INCREMENT_ASYNC')} />,
     document.getElementById('root')
   )
 }
